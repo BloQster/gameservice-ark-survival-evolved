9ec9ec2dfebfcd4d2e8cb23b5192b07027328e22
Michael Bortlik <michael.bortlik@gmail.com>
2017-02-12 17:57:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 arkmanager upgrade-tools
+arkmanager install --verbose
 if [ ! -f "/home/steam/ark_installed" ]
 then
-	echo "INSTALL ARK"
-	arkmanager install --verbose
+	echo "INSTALL ARK"	
 	arkmanager install-cronjob --enable-output --hour="*/3" update --warn --backup
 	touch "/home/steam/ark_installed"
 fi 
-
 arkmanager update --verbose
 arkmanager run --verbose
