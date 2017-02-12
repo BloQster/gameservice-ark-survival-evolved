f6bb26c1f18c00a2f5550e2f6a025f8d4278c009
Michael Bortlik <michael.bortlik@gmail.com>
2017-02-12 18:12:37 +0100

    added backup cronjob

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ if [ ! -f "/home/steam/ark_installed" ]
 then
 	echo "INSTALL ARK"	
 	arkmanager install-cronjob --enable-output --hour="*/3" update --warn --backup
+	arkmanager install-cronjob --hourly backup
 	touch "/home/steam/ark_installed"
 fi 
 arkmanager update --verbose
